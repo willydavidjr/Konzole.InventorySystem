c57c3cda9f5a29e8bc7d03b23a68faf14a2eaab3
willydavidjr <willydavidjr@yahoo.com>
2017-06-17 21:47:35 +0800

    Login initial.

--- Konzole.InventorySystem.Web/Controllers/AccountController.cs
+++ Konzole.InventorySystem.Web/Controllers/AccountController.cs
@@ -73,6 +73,7 @@ namespace Konzole.InventorySystem.Web.Controllers
                 return View(model);
             }
 
+            
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
